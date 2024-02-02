bad3433ba33d3b8d89357e42efa0d0b9a7abbe20
81806 <81806@YOSHITO>
2024-02-02 23:30:22 +0900

    プロジェクト ファイルを追加します。

---
+++ テスト/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace テスト
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 95);
+            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(164, 47);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            button1.MouseDown += button1_MouseDown;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(27, 146);
+            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(164, 47);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.MouseDown += button2_MouseDown;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(27, 196);
+            button3.Margin = new Padding(2, 2, 2, 2);
+            button3.Name = "button3";
+            button3.Size = new Size(164, 47);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.MouseDown += button3_MouseDown;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(27, 247);
+            button4.Margin = new Padding(2, 2, 2, 2);
+            button4.Name = "button4";
+            button4.Size = new Size(164, 47);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.MouseDown += button4_MouseDown;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(27, 297);
+            button5.Margin = new Padding(2, 2, 2, 2);
+            button5.Name = "button5";
+            button5.Size = new Size(164, 47);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.MouseDown += button5_MouseDown;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(27, 347);
+            button6.Margin = new Padding(2, 2, 2, 2);
+            button6.Name = "button6";
+            button6.Size = new Size(164, 47);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            button6.MouseDown += button6_MouseDown;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(27, 398);
+            button7.Margin = new Padding(2, 2, 2, 2);
+            button7.Name = "button7";
+            button7.Size = new Size(164, 47);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            button7.MouseDown += button7_MouseDown;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(27, 448);
+            button8.Margin = new Padding(2, 2, 2, 2);
+            button8.Name = "button8";
+            button8.Size = new Size(164, 47);
+            button8.TabIndex = 7;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            button8.MouseDown += button8_MouseDown;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(27, 499);
+            button9.Margin = new Padding(2, 2, 2, 2);
+            button9.Name = "button9";
+            button9.Size = new Size(164, 47);
+            button9.TabIndex = 8;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            button9.MouseDown += button9_MouseDown;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(27, 549);
+            button10.Margin = new Padding(2, 2, 2, 2);
+            button10.Name = "button10";
+            button10.Size = new Size(164, 47);
+            button10.TabIndex = 9;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            button10.MouseDown += button10_MouseDown;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(27, 26);
+            button11.Margin = new Padding(2, 2, 2, 2);
+            button11.Name = "button11";
+            button11.Size = new Size(52, 58);
+            button11.TabIndex = 10;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            button11.MouseDown += button11_MouseDown;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(83, 26);
+            button12.Margin = new Padding(2, 2, 2, 2);
+            button12.Name = "button12";
+            button12.Size = new Size(52, 58);
+            button12.TabIndex = 11;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            button12.MouseDown += button12_MouseDown;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(139, 26);
+            button13.Margin = new Padding(2, 2, 2, 2);
+            button13.Name = "button13";
+            button13.Size = new Size(52, 58);
+            button13.TabIndex = 12;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            button13.MouseDown += button13_MouseDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
+            ClientSize = new Size(223, 616);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(2, 2, 2, 2);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "よしとオリジナル";
+            Activated += Form1_Activated;
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+    }
+}
---
+++ テスト/Form3.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace テスト
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 21);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 0;
+            label1.Text = "タブ名：1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 79);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 1;
+            label2.Text = "タブ名：3";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 47);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 15);
+            label3.TabIndex = 2;
+            label3.Text = "タブ名：2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(140, 21);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(106, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(140, 47);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(106, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(140, 79);
+            textBox3.Margin = new Padding(2, 2, 2, 2);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(106, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(140, 125);
+            button1.Margin = new Padding(2, 2, 2, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 20);
+            button1.TabIndex = 6;
+            button1.Text = "保存";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(249, 125);
+            button2.Margin = new Padding(2, 2, 2, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(78, 20);
+            button2.TabIndex = 7;
+            button2.Text = "戻る";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(560, 270);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "タブ名編集しろ";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+    }
+}
