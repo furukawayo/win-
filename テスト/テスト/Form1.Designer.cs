5a6f7afa4af56559ed999a052e80cc75b8f22ed7
ふるかわ <96404818+furukawayo@users.noreply.github.com>
2025-06-04 00:37:09 +0900

    Add files via upload

---
+++ テスト/テスト/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace テスト
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(234, 78);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.MouseDown += button1_MouseDown;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(39, 243);
+            button2.Name = "button2";
+            button2.Size = new Size(234, 78);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.MouseDown += button2_MouseDown;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(39, 327);
+            button3.Name = "button3";
+            button3.Size = new Size(234, 78);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.MouseDown += button3_MouseDown;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(39, 411);
+            button4.Name = "button4";
+            button4.Size = new Size(234, 78);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.MouseDown += button4_MouseDown;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(39, 495);
+            button5.Name = "button5";
+            button5.Size = new Size(234, 78);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.MouseDown += button5_MouseDown;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(39, 579);
+            button6.Name = "button6";
+            button6.Size = new Size(234, 78);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            button6.MouseDown += button6_MouseDown;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(39, 663);
+            button7.Name = "button7";
+            button7.Size = new Size(234, 78);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            button7.MouseDown += button7_MouseDown;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(39, 747);
+            button8.Name = "button8";
+            button8.Size = new Size(234, 78);
+            button8.TabIndex = 7;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            button8.MouseDown += button8_MouseDown;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(39, 831);
+            button9.Name = "button9";
+            button9.Size = new Size(234, 78);
+            button9.TabIndex = 8;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            button9.MouseDown += button9_MouseDown;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(39, 915);
+            button10.Name = "button10";
+            button10.Size = new Size(234, 78);
+            button10.TabIndex = 9;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            button10.MouseDown += button10_MouseDown;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(39, 44);
+            button11.Name = "button11";
+            button11.Size = new Size(74, 96);
+            button11.TabIndex = 10;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            button11.MouseDown += button11_MouseDown;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(119, 44);
+            button12.Name = "button12";
+            button12.Size = new Size(74, 96);
+            button12.TabIndex = 11;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            button12.MouseDown += button12_MouseDown;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(199, 44);
+            button13.Name = "button13";
+            button13.Size = new Size(74, 96);
+            button13.TabIndex = 12;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            button13.MouseDown += button13_MouseDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(318, 1027);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            ForeColor = SystemColors.InactiveCaptionText;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ふるかわコピペ.in";
+            Activated += Form1_Activated;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+    }
+}
